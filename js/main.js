10b440f57dcdcb325592cfd5f5e1822e8137d499
ESHONQUL08012003 <abdulazizoveshonqul66@gmail.com>
2022-09-27 22:31:22 +0500

    initiial commit

--- js/main.js
+++ js/main.js
@@ -1,11 +1,40 @@
 let tbody = document.querySelector(".tbody");
+let elImg = document.querySelector(".button__search");
+let elNav = document.querySelector(".header__nav");
+let inputSearch = document.querySelector(".search-header");
+
+elImg.onclick = function () {
+  inputSearch.classList.add("search-header-active");
+  this.classList.add("search-button-none");
+  inputSearch.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+      inputSearch.classList.remove("search-header-active");
+      elImg.classList.remove("search-button-none");
+      elImg.classList.add("search-button-active");
+    }
+  });
+};
+
+const colorFunction = (names) => {
+  let red = "red";
+  let green = "green";
+  let yellow = "yellow";
+
+  if (name === "normal") {
+    return red;
+  } else if (name === "hight") {
+    return green;
+  } else if (nmae === "low") {
+    return yellow;
+  }
+};
 
 function renderUser(alluser) {
   alluser.forEach((element) => {
     let newTr = document.createElement("tr");
     newTr.innerHTML = `
       <td>
-        <div class="costum">
+        <div class="d-flex gap-4">
          <img class = "avatar-img" src="${element.ava}" alt="${element.name}">
           <div>
           <strong>${element.company}</strong>
@@ -32,10 +61,11 @@ function renderUser(alluser) {
 
       <td>
         <button class="imgs-btn">
-        <img src="./images/more - vertical.svg" alt="">
+        <img src="./images/more-vertical.svg" alt="">
         </button>
        </td>
     `;
+    console.log(newTr);
     tbody.appendChild(newTr);
   });
 }
